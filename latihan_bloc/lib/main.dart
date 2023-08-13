c4abb2808089b6254037cb0524d0aa7ebfaef958
arqomanwar <arkomanwar@gmail.com>
2023-08-13 12:20:34 +0700

    konsep blocconsumer

--- latihan_bloc/lib/main.dart
+++ latihan_bloc/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'lessons/bloc_listener_page.dart';
+import 'lessons/bloc_consumer_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BlocListenerPage(),
+      home: BlocConsumerPage(),
     );
   }
 }
